af4106a2aff90de9ad6c2b35f1a1c4e18e51baf0
Pierry Borges <pieerry@gmail.com>
2015-12-29 15:17:00 -0200

    Update Conta.java

--- src/banco/Conta.java
+++ src/banco/Conta.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public abstract class Conta {
 
-    int IDconta;
-    protected double saldo;
-    int ClienteID;
+    int idConta;
+    int clienteId;
+    double saldo;
     Scanner Leitura = new Scanner(System.in);
 
-    public void Saca(double valor) {
+    public void saca(double valor) {
         if (saldo < 0) {
             System.out.println("conta negativa não podera efetuar saques");
             this.saldo = 0;
@@ -18,7 +18,7 @@ public abstract class Conta {
         }
     }
     
-    public Double VerSaldo(){
+    public Double verSaldo(){
         return this saldo;
     }
 }
