f2fb4ffc0c640a42692f70e7d37124c7748cb05c
Lucas Selliach <lucas.selliach@outlook.com>
2015-12-29 15:00:41 -0200

    Update conta alterando para ver saldo

--- src/banco/Conta.java
+++ src/banco/Conta.java
@@ -10,13 +10,15 @@ public abstract class Conta {
     Scanner Leitura = new Scanner(System.in);
 
     public void Saca(double valor) {
-        System.out.println("Valor para o saque?");
-        Leitura.nextDouble();
         if (saldo < 0) {
             System.out.println("conta negativa não podera efetuar saques");
             this.saldo = 0;
         } else {
             valor -= this.saldo;
         }
+    }
+    
+    public Double VerSaldo(){
+        return this saldo;
     }
 }
