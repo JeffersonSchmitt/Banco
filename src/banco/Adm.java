40958b5c80af79399fe513b50f30e07bfa1090a8
Pierry Borges <pieerry@gmail.com>
2015-12-29 15:10:45 -0200

    Update Adm.java

--- src/banco/Adm.java
+++ src/banco/Adm.java
@@ -7,4 +7,8 @@ public class Adm extends Cliente {
     public void Cadastro(String cadastro) {
         this.Ncadastro = cadastro;
     }
+    
+    public String getNCadastro(){
+        return this.Ncadastro;
+    }
 }
