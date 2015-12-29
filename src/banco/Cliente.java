040428bf3acc93c081affb4c2155c4dbb4e6ddfe
Lucas Selliach <lucas.selliach@outlook.com>
2015-12-29 14:51:36 -0200

    Alteração de cliente para deixar de ser anemico

--- src/banco/Cliente.java
+++ src/banco/Cliente.java
@@ -2,11 +2,25 @@ package banco;
 
 public class Cliente extends Conta implements IVerificacao {
 
-    int ID;
-    String Nome;
-    int Anodenascimento;
-    String CPF;
-    String Telefone;
+    int ID { get; private set }
+    String Nome { get; private set }
+    string dataAniversario { get; private set }
+    String CPF { get; private set }
+    String Telefone { get; private set }
+
+    public Cliente(int id, string nome, string dataAniversario, string cpf)
+    {
+        this.ID = id;
+        this.Nome = nome;
+        this.dataAniversario = dataAniversario;
+        this.CPF = cpf;
+    }
+    
+    public SetTelefone(string telefone){
+        //fazer validações
+        
+        this.Telefone = telefone;
+    }
 
     public Boolean verifica() {
         if (this.ID != ID) {
